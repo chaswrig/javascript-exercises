23ded3f1506f5c384eb1ea08032990987277e31f
chaswrig <42905430+chaswrig@users.noreply.github.com>
2024-05-17 11:09:20 -0500

    Solution 1 -- RepeatString passes all tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(input, number) {
+    if(number < 0) {
+        return 'ERROR';
+    } else {
+        let output = '';
+        for(i = 1; i <= number; i++) {
+            output = output + input;
+        }
+        
+        return output;
+    }
 };
 
 // Do not edit below this line
