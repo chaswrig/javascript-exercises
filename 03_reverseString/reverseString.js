99b5165da2c0ae2a511ddd406205ebb26888d615
chaswrig <42905430+chaswrig@users.noreply.github.com>
2024-05-17 11:36:49 -0500

    Solution 03 -- Reversed a string. Not as elegant as proper solution, but it works.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let length = input.length -1;
+    let output = '';
+    for(i = length; i >= 0; i--){
+        output = output + input[i];
+    }
 
+    return output;
 };
 
 // Do not edit below this line
