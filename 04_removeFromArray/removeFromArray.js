3213ec24cd663771b6329a9b6c86dbf288cf6477
chaswrig <42905430+chaswrig@users.noreply.github.com>
2024-05-17 22:12:38 -0500

    It's not pretty, but it works.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(startingArray) {
+    
+    for(i = 1; i < arguments.length; i++){
+        let numberLoops = startingArray.length
+        for(j = 1; j <= numberLoops;j++){
+            const index = startingArray.indexOf(arguments[i]);
+            if (index >-1) {
+                startingArray.splice(index,1);
+            }
+        }
+    }
 
+    return startingArray;
 };
+
+//argument.length will return the number of objects to be removed + 1
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
