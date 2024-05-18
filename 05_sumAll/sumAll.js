c0f5cacc368d280668c7abd53c4b02f51e932822
chaswrig <42905430+chaswrig@users.noreply.github.com>
2024-05-17 22:28:58 -0500

    Solution 5 -- Works, but has duplicate code. Suggested solution is cleaner.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    let firstNumberType = typeof firstNumber;
+    let secondNumberType = typeof secondNumber;
+    if (firstNumberType !== "number" || secondNumberType !== "number"){
+        return "ERROR";
+    } else if (firstNumber < 0 || secondNumber < 0){
+        return "ERROR";
+    } else {
+        let sumAll = 0;
+        if (firstNumber < secondNumber) {
+            for(i = firstNumber; i <= secondNumber; i++) {
+                sumAll = sumAll + i;
+            }
+        } else {
+            for(i = secondNumber; i <= firstNumber; i++){
+                sumAll = sumAll + i;
+            }
+        }
 
+        return sumAll;
+    }
 };
 
 // Do not edit below this line
