96ce59f366d9ee789674547b8e594105df6d20a8
chaswrig <42905430+chaswrig@users.noreply.github.com>
2024-05-18 13:13:57 -0500

    Solution 6 -- Uses loops instead of one big conditional.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    divisibleByFour = year % 4;
+    divisibleByHundred = year % 100;
+    divisiblebyFourHundred = year % 400
+    if (divisibleByHundred == 0 && divisibleByFour == 0 && divisiblebyFourHundred == 0){
+        return true;
+    } else if(divisibleByHundred == 0 && divisiblebyFourHundred != 0){
+        return false;
+    } else if(divisibleByHundred != 0 && divisibleByFour == 0){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
