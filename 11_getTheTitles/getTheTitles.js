c666e09628a8df5f8958dc2e40a19b820d291440
chaswrig <42905430+chaswrig@users.noreply.github.com>
2024-05-23 14:03:43 -0500

    Solution 11 -- Submitted solution 11. Stopped work on solution 12."

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    books.forEach((book) => {
+        let title = book["title"];
+        titles.push(title);
+    })
+    return titles;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    //Set the year to the year of the computer running code.
+    let date = new Date();
+    date.setTime(Date.now());
+    const currentYear = date.getFullYear();
 
+    //Figure out who is still alive, set death date to this year for easy math
+    const stillAlive = people.filter(isAlive);     
+
+    return;
 };
+
+function hasDied(person){
+    let key = "yearOfDeath";
+    return (key in person);
+}
+
+function isAlive(person){
+    let key = "yearOfDeath";
+    if((key in person) == false){
+        return true;
+    };
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
