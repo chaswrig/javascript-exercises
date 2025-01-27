d7db97f4c0ca666fe645663f9105608d9e0884c3
chaswrig <42904530+chaswrig@users.noreply.github.com>
2025-01-27 13:15:43 -0600

    Solutions 1, 2, 3 complete.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let outputString = '';
+    for (let length = inputString.length - 1; length >= 0; length--){
+        outputString += inputString.charAt(length);
+    }
+    return outputString;
 };
 
 // Do not edit below this line
